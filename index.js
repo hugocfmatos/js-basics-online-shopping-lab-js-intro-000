61eb172452124ae8f3520fdbece51425c4dddf2f
hugo matos <hugocfmatos@gmail.com>
2020-09-18 16:52:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,9 +56,9 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber.length===0 || cardNumber==="") {
     return "Sorry, we don't have a credit card on file for you."
-  } else {
+  }
     var newTotal = total()
     cart.length=0
     return `Your total cost is $${newTotal}, which will be charged to the card ${cardNumber}.`
-}
+
 }
