f356b7b67d7c9371d9807e645ffcaec21e4f908e
hugo matos <hugocfmatos@gmail.com>
2020-09-18 16:50:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber==="") {
+  if (cardNumber.length===0 || cardNumber==="") {
     return "Sorry, we don't have a credit card on file for you."
   } else {
     var newTotal = total()
