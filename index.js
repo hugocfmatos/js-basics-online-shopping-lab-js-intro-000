45ab2a079f7ff0cf15a0cead188aa8eabfb8f2ba
hugo matos <hugocfmatos@gmail.com>
2020-09-18 15:55:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  newItem={itemName:item, itemPrice: Math.floor(Math.random()*100)}
+  cart.push({itemName:item, itemPrice: Math.floor(Math.random()*100)})
   return `${name} has been added to your cart.`
 }
 
